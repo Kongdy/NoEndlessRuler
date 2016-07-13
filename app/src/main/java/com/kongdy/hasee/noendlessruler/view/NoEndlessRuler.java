513afa7ef4b7ac451a302376b67c406c08a779bc
kongdy <906011748@qq.com>
2016-07-13 18:37:37 +0800

    aaa

--- app/src/main/java/com/kongdy/hasee/noendlessruler/view/NoEndlessRuler.java
+++ app/src/main/java/com/kongdy/hasee/noendlessruler/view/NoEndlessRuler.java
@@ -1,10 +1,16 @@
 package com.kongdy.hasee.noendlessruler.view;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * 尺子,无限瀑布流
@@ -13,35 +19,332 @@ import android.util.AttributeSet;
  * @TIME 20:13
  **/
 
-public class NoEndlessRuler extends RecyclerView {
+public class NoEndlessRuler extends FrameLayout {
+
+    private int mHeight;
+    private int mWidth;
+
+    private ORIENTATION mOrientation;
+
+    private boolean beginDraged = false;
+
+    private float mLastMotionX;
+    private float mLastMotionY;
+
+    private float mTouSlop;// 最小滑动阀值
+    private float mOverscrollDistance;
+
+    private int mScrollY;
+    private int mScrollX;
+
+    private Paint mPointerPaint; // 指针画笔
+
+    private DeadRuler ruler;
+
+    private int pointerId;
+
+    public NoEndlessRuler(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public NoEndlessRuler(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
     public NoEndlessRuler(Context context) {
         super(context);
         init();
     }
 
-    public NoEndlessRuler(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
+    private void init() {
+        ViewConfiguration configuration = ViewConfiguration.get(getContext());
+        mTouSlop = configuration.getScaledPagingTouchSlop();
+        mOverscrollDistance = configuration.getScaledOverscrollDistance();
+
+        pointerId = 1;
+
+        ruler = new DeadRuler(getContext());
+        // 默认水平滑动
+        setmOrientation(ORIENTATION.HORIZONTAL);
+
+        addView(ruler, ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        mPointerPaint = new Paint();
+
+        mPointerPaint.setAntiAlias(true);
+
+        mPointerPaint.setColor(Color.argb(255, 250, 124, 0));
+
+        mPointerPaint
+                .setStrokeWidth(getRawSize(TypedValue.COMPLEX_UNIT_DIP, 2));
+
+        mPointerPaint.setStyle(Paint.Style.STROKE);
     }
 
-    public NoEndlessRuler(Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
+    @Override
+    protected void measureChildWithMargins(View child,
+                                           int parentWidthMeasureSpec, int widthUsed,
+                                           int parentHeightMeasureSpec, int heightUsed) {
+        if (mOrientation == ORIENTATION.VERTICAL) {
+            final MarginLayoutParams marginLayoutParams = (MarginLayoutParams) child
+                    .getLayoutParams();
+            child.measure(parentWidthMeasureSpec, MeasureSpec.makeMeasureSpec(
+                    marginLayoutParams.topMargin
+                            + marginLayoutParams.bottomMargin,
+                    MeasureSpec.UNSPECIFIED));
+        } else {
+            final MarginLayoutParams marginLayoutParams = (MarginLayoutParams) child
+                    .getLayoutParams();
+            child.measure(MeasureSpec.makeMeasureSpec(
+                    marginLayoutParams.leftMargin
+                            + marginLayoutParams.rightMargin,
+                    MeasureSpec.UNSPECIFIED), parentHeightMeasureSpec);
+        }
     }
 
     /**
-     * 初始化
+     * <h1>
+     * 滑动处理</h1> 触摸事件写的比较简陋，以后完善
      */
-    private void init() {
-
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                mLastMotionX = ev.getRawX();
+                mLastMotionY = ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 两套滑动机制
+                int deltaX = 0;
+                int deltaY = 0;
+                int overScrollDistanceX = 0;
+                int overScrollDistanceY = 0;
+                int scrollRangeX = 0;
+                int scrollRangeY = 0;
+                if (mOrientation == ORIENTATION.HORIZONTAL) {
+                    final float moveX = ev.getRawX();
+                    deltaX = (int) (mLastMotionX - moveX);
+                    if (!beginDraged && Math.abs(deltaX) > mTouSlop) {
+                        beginDraged = true;
+                    }
+                    mScrollX = getScrollX() + deltaX;
+                    mScrollY = getScrollY();
+                    scrollRangeX = getScrollRangeX();
+                    overScrollDistanceX = (int) mOverscrollDistance;
+                } else {
+                    final float moveY = ev.getRawY();
+                    deltaY = (int) (mLastMotionY - moveY);
+                    if (!beginDraged && Math.abs(deltaY) > mTouSlop) {
+                        beginDraged = true;
+                    }
+                    mScrollX = getScrollX();
+                    mScrollY = getScrollY() + deltaY;
+                    scrollRangeY = getScrollRangeY();
+                    overScrollDistanceY = (int) mOverscrollDistance;
+                }
+                if (beginDraged) {
+                    overScrollBy(deltaX, deltaY, mScrollX, mScrollY, scrollRangeX,
+                            scrollRangeY, overScrollDistanceX, overScrollDistanceY,
+                            true);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                beginDraged = false;
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 
+    /**
+     * 滑动算法后期有待优化
+     */
+    @Override
+    protected void onOverScrolled(int scrollX, int scrollY, boolean clampedX,
+                                  boolean clampedY) {
+        // super.onOverScrolled(scrollX, scrollY, clampedX, clampedY);
+        super.scrollTo(scrollX, scrollY);
+    }
+
+    /**
+     * 判断是否到最底部
+     *
+     * @return
+     */
+    private int getScrollRangeX() {
+        int scrollRangX = 0;
+        if (getChildCount() > 0 && mOrientation == ORIENTATION.HORIZONTAL) {
+            View child = getChildAt(0);
+            scrollRangX = Math.max(0, child.getWidth()
+                    - (getWidth() - getPaddingLeft() - getPaddingRight()));
+        }
+        return scrollRangX;
+    }
+
+    /**
+     * 判断是否到最右边
+     *
+     * @return
+     */
+    private int getScrollRangeY() {
+        int scrollRangY = 0;
+        if (getChildCount() > 0 && mOrientation == ORIENTATION.VERTICAL) {
+            View child = getChildAt(0);
+            scrollRangY = Math.max(0, child.getHeight()
+                    - (getHeight() - getPaddingTop() - getPaddingBottom()));
+        }
+        return scrollRangY;
+    }
 
     @Override
-    public void onDraw(Canvas c) {
-        super.onDraw(c);
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+        drawPointer();
     }
 
+    @Override
+    public void addView(View child) {
+        if (getChildCount() > 1) {
+            throw new IllegalStateException(
+                    "NoEndlessRuler can host only one direct child");
+        }
+        super.addView(child);
+    }
+
+    @Override
+    public void addView(View child, int index,
+                        android.view.ViewGroup.LayoutParams params) {
+        if (getChildCount() > 1) {
+            throw new IllegalStateException(
+                    "NoEndlessRuler can host only one direct child");
+        }
+        super.addView(child, index, params);
+    }
+
+    @Override
+    public void addView(View child, int width, int height) {
+        if (getChildCount() > 1) {
+            throw new IllegalStateException(
+                    "NoEndlessRuler can host only one direct child");
+        }
+        super.addView(child, width, height);
+    }
+
+    @Override
+    public void addView(View child, int index) {
+        if (getChildCount() > 1) {
+            throw new IllegalStateException(
+                    "NoEndlessRuler can host only one direct child");
+        }
+        super.addView(child, index);
+    }
+
+    @Override
+    public void addView(View child, android.view.ViewGroup.LayoutParams params) {
+        if (getChildCount() > 1) {
+            throw new IllegalStateException(
+                    "NoEndlessRuler can host only one direct child");
+        }
+        super.addView(child, params);
+    }
+
+    /**
+     * 方向枚举
+     *
+     * @author wangk
+     */
+    public static enum ORIENTATION {
+        VERTICAL, HORIZONTAL
+    }
+
+    public ORIENTATION getmOrientation() {
+        return mOrientation;
+    }
+
+    public void setmOrientation(ORIENTATION mOrientation) {
+        this.mOrientation = mOrientation;
+        ruler.setmOrientation(mOrientation);
+    }
+
+    /**
+     * 根据单位返回一个像素
+     *
+     * @param context
+     * @param unit
+     * @param value
+     * @return
+     */
+    public float getRawSize(int unit, float value) {
+        DisplayMetrics metrics = getContext().getResources()
+                .getDisplayMetrics();
+        return TypedValue.applyDimension(unit, value, metrics);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+                            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        View child = getChildAt(0);
+        // 置中
+        if (mOrientation == ORIENTATION.HORIZONTAL) {
+            super.setScrollX((Math.abs(getWidth() - child.getWidth())) / 2);
+        } else {
+            super.setScaleY((Math.abs(getHeight() - child.getHeight()) / 2));
+        }
+
+
+    }
+
+    /**
+     * 绘制指针
+     */
+    private void drawPointer() {
+        ViewGroup parent = ((ViewGroup)getParent());
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            if(parent.getChildAt(i).getId() == pointerId) {
+                return;
+            }
+        }
+        View view = new View(getContext());
+        view.setBackgroundColor(Color.argb(255, 250, 124, 0));
+        view.setId(pointerId);
+        int l = 0;
+        int t = 0;
+        int r = 0;
+        int b = 0;
+        if (mOrientation == ORIENTATION.HORIZONTAL) {
+            l = (int) (getLeft() + mWidth / 2 - getRawSize(
+                    TypedValue.COMPLEX_UNIT_DIP, 1));
+            t = getTop() + mHeight / 3;
+            r = (int) (getLeft() + mWidth / 2 + getRawSize(
+                    TypedValue.COMPLEX_UNIT_DIP, 1));
+            b = getBottom() - ruler.getBottomLabelPadding();
+            parent.addView(view,new LayoutParams((int) getRawSize(
+                    TypedValue.COMPLEX_UNIT_DIP, 2), mHeight / 3));
+        } else {
+            l = getLeft() -ruler.getBottomLabelPadding();
+            t = (int) (getTop()+mHeight/2-getRawSize(
+                    TypedValue.COMPLEX_UNIT_DIP, 1));
+            r = getRight()-mWidth/3;
+            b = (int) (getBottom()-mHeight/2+getRawSize(
+                    TypedValue.COMPLEX_UNIT_DIP, 1));
+            parent.addView(view,new LayoutParams(mWidth/3, (int) getRawSize(
+                    TypedValue.COMPLEX_UNIT_DIP, 2)));
+        }
+
+
+        MarginLayoutParams marginLayoutParams = (MarginLayoutParams) view.getLayoutParams();
+        marginLayoutParams.leftMargin = l;
+        marginLayoutParams.rightMargin = r;
+        marginLayoutParams.topMargin = t;
+        marginLayoutParams.bottomMargin = b;
+        view.setLayoutParams(marginLayoutParams);
+    }
 
 }
